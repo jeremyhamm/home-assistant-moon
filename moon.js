d9f81e4a37a073bc7dfbcb63d33355092a332c50
Jeremy Hamm <jh@jeremyhamm.dev>
2020-09-11 22:35:19 -0700

    Add card template

---
+++ moon.js
@@ -0,0 +1,74 @@
+import "https://unpkg.com/dayjs@1.8.21/dayjs.min.js";
+
+const loadCSS = (url) => {
+  const link = document.createElement("link");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = url;
+  document.head.appendChild(link);
+}
+
+class MoonPhasesCard {
+  static get properties() {
+    return {
+      hass: {},
+      config: {}
+    };
+  }
+
+  render() {
+    const now = dayjs().format();
+    console.log(now);
+    return html`
+      <div>
+        
+      </div>
+    `;
+  }
+
+  setConfig(config) {
+    this.config = config;
+  }
+
+  // The height of your card. Home Assistant uses this to automatically
+  // distribute all cards over the available columns.
+  getCardSize() {
+    return this.config.entities.length + 1;
+  }
+
+  _toggle(state) {
+    this.hass.callService("homeassistant", "toggle", {
+      entity_id: state.entity_id
+    });
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        font-family: "Gloria Hallelujah", cursive;
+      }
+      wired-card {
+        background-color: white;
+        padding: 16px;
+        display: block;
+        font-size: 18px;
+      }
+      .state {
+        display: flex;
+        justify-content: space-between;
+        padding: 8px;
+        align-items: center;
+      }
+      .not-found {
+        background-color: yellow;
+        font-family: sans-serif;
+        font-size: 14px;
+        padding: 8px;
+      }
+      wired-toggle {
+        margin-left: 8px;
+      }
+    `;
+  }
+}
+customElements.define("moon", MoonPhasesCard);
