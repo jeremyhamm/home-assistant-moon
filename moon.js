4d17f1af8bf04f4879fa4524edd39c6f202f5c9f
Jeremy Hamm <jeremyhamm@users.noreply.github.com>
2020-10-03 17:27:30 -0700

    Update moon.js
    Add phase calculation and normalization.

--- moon.js
+++ moon.js
@@ -1,60 +1,116 @@
-import "https://unpkg.com/dayjs@1.8.21/dayjs.min.js";
-
-class MoonPhasesCard {
-  static get properties() {
-    return {
-      hass: {},
-      config: {}
-    };
-  }
-
-  render() {
-    const now = dayjs().format();
-    console.log(now);
-    return html`
-      <div>
-        
-      </div>
-    `;
-  }
-
-  setConfig(config) {
-    this.config = config;
-  }
-
-  // The height of your card. Home Assistant uses this to automatically
-  // distribute all cards over the available columns.
-  getCardSize() {
-    return this.config.entities.length + 1;
-  }
-
-  static get styles() {
-    return css`
-      :host {
-        font-family: "Gloria Hallelujah", cursive;
-      }
-      wired-card {
-        background-color: white;
-        padding: 16px;
-        display: block;
-        font-size: 18px;
-      }
-      .state {
-        display: flex;
-        justify-content: space-between;
-        padding: 8px;
-        align-items: center;
-      }
-      .not-found {
-        background-color: yellow;
-        font-family: sans-serif;
-        font-size: 14px;
-        padding: 8px;
-      }
-      wired-toggle {
-        margin-left: 8px;
-      }
-    `;
-  }
+/**
+ * Normalize moon phase number
+ * 
+ * @param {String} phase
+ * 
+ * @return {String}
+ */
+const normalizeMoonPhase = (phase) => {
+    switch(phase) {
+        case 0:
+            return {
+                id: 'new-moon',
+                name: 'New Moon',
+            };
+        case 1:
+            return {
+                id: 'waxing-crescent-moon',
+                name:'Waxing Crescent Moon',
+            };
+        case 2:
+            return {
+                id: 'first-quarter-moon',
+                name: 'First Quarter Moon',
+            };
+        case 3:
+            return {
+                id: 'waxing-gibbous-moon',
+                name: 'Waxing Gibbous Moon',
+            };
+        case 4:
+            return {
+                id: 'full-moon',
+                name: 'Full Moon',
+            };
+        case 5:
+            return {
+                id: 'waning-gibbous-moon',
+                name: 'Waning Gibbous Moon',
+            };
+        case 6:
+            return {
+                id: 'last-quarter-moon',
+                name: 'Last Quarter Moon'
+            };
+        case 7:
+            return {
+                id: 'waning-crescent-moon',
+                name:'Waning Crescent Moon'
+            };
+    }
 }
-customElements.define("moon", MoonPhasesCard);
+
+/**
+ * Get current moon phase
+ * Thanks https://gist.github.com/endel/dfe6bb2fbe679781948c
+ * 
+ * @param {Number} year
+ * @param {Number} month
+ * @param {Number} day
+ * 
+ * @return {String}
+ */
+const getMoonPhase = (year, month, day) => {
+    let c, e, jd, b = 0;
+    if ( month < 3 ) {
+        year--;
+        month += 12;
+    }
+    ++month;
+    c = 365.25 * year;
+    e = 30.6 * month;
+    jd = c + e + day - 694039.09; //jd is total days elapsed
+    jd /= 29.5305882; //divide by the moon cycle
+    b = parseInt(jd); //int(jd) -> b, take integer part of jd
+    jd -= b; //subtract integer part to leave fractional part of original jd
+    b = Math.round(jd * 8); //scale fraction from 0-8 and round
+    if ( b >= 8 ) {
+        b = 0; //0 and 8 are the same so turn 8 into 0
+    }
+    
+    return normalizeMoonPhase(b);
+}
+
+/**
+ * Phases of the moon custom lovelace UI card
+ */
+class MoonPhasesCard extends HTMLElement {
+    set hass(hass) {
+        if (!this.content) {
+            const card = document.createElement('ha-card');
+            card.header = phase.name;
+            this.content = document.createElement('div');
+            this.content.style.padding = '0 16px 16px';
+            card.appendChild(this.content);
+            this.appendChild(card);
+      
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = now.getMonth() + 1;
+            const day = now.getDay();
+            const phase = getMoonPhase(year, month, day);
+            
+            console.log(`Current phase - ${phase.name} 🌙`);
+            
+            this.content.innerHTML = `
+                <img src="/local/moon-phases/images/${phase.id}.png">
+            `;
+        }
+    }
+
+    setConfig(config) {
+        this.config = config;
+    }
+}
+
+customElements.define('moon-phases', MoonPhasesCard);
